29ecc135e9cd53c1228889e3eb45779a0437ec9b
Tainara Victória <82177319+tainaravctr@users.noreply.github.com>
2023-08-03 11:24:24 -0300

    Update ExercicioLaco.java

--- gen/ExercicioLaco.java
+++ gen/ExercicioLaco.java
@@ -1,1 +1,42 @@
+package ExercicioUm;
 
+import java.util.Scanner;
+
+public class ExercicioLaco {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		int A = 0,B = 0,C;
+		int adicao = A+B;
+
+		Scanner leia = new Scanner(System.in);
+		
+		
+		
+		System.out.println("Digite o valor A: ");
+		A = (int) leia.nextFloat();
+		
+		System.out.println("Digite o valor B: ");
+		B = (int) leia.nextFloat();
+		
+		System.out.println("Digite o valor C: ");
+		C = (int) leia.nextFloat();
+		
+		
+		if(adicao>C) {
+			System.out.println("\nA Soma de A + B é maior do que C");
+		} 
+		
+		else if (adicao == C) {
+			System.out.println("\nA Soma de A + B é Igual a C");
+		}
+		
+		else {
+			System.out.println("\nA Soma de A + B é Menor do que C");
+		}
+		
+	}
+
+}
